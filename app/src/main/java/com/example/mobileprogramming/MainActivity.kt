6455718541bcc8912a0811c07e5c55e78cced02f
rekafekili <rekafekili@gmail.com>
2020-03-18 00:15:25 +0900

    Week 1 : Print My name and number

---
+++ app/src/main/java/com/example/mobileprogramming/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mobileprogramming
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView : TextView = findViewById(R.id.textview)
+
+        textView.setText("14학번 조성윤")
+    }
+}
