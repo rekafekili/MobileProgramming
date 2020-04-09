72d85d66f05818debe06bc3a58b7a4b127464e7d
Rekafekili <rekafekili@gmail.com>
2020-04-10 04:12:38 +0900

    Week 4 : Add Light App

--- LightApp/app/src/main/java/com/example/lightapp/MainActivity.kt
+++ LightApp/app/src/main/java/com/example/lightapp/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.lightapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+/**
+ * 한국기술교육대 2020년 1학기 모바일 프로그래밍 조성윤
+ *
+ * LightApp
+ * 여백을 없애기
+ * 앱 이름(LightApp)이 나오지 않도록
+ * 스위치를 없애고, 화면이 터치하면 ON, OFF 번갈아가며 바뀌도록
+ */
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var isLight = false
+        framelayout.setOnClickListener {
+            if(isLight) {
+                light_imageview.background = getDrawable(R.drawable.off)
+                isLight = false
+            } else {
+                light_imageview.background = getDrawable(R.drawable.on)
+                isLight = true
+            }
+        }
     }
 }
