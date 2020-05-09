2daf4965a91436905e3745f9570572f1f2d57942
rekafekili <rekafekili@gmail.com>
2020-05-10 04:28:30 +0900

    Week 8.1.3 : Service 만들기와 Ringtone 사용

--- AlarmApp/app/src/main/java/com/example/alarmapp/AlarmReceiver.kt
+++ AlarmApp/app/src/main/java/com/example/alarmapp/AlarmReceiver.kt
@@ -8,10 +8,18 @@ import android.util.Log
 class AlarmReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
+        val receivedState = intent.getStringExtra("state")
+
         // Broadcast 메시지를 수신하면 intent 와 함께 실행된다.
         // Case 1 : AlarmManager가 Broadcast
         // Case 2 : 직접 sendBroadcast() 해서 Broadcast
         Log.d("AlarmReceiver", "onReceive() Called")
-        Log.d("AlarmReceiver", "Intent Extra : ${intent.getStringExtra("state")}")
+        Log.d("AlarmReceiver", "Intent Extra : $receivedState")
+
+        // state 값(ON, OFF)에 따라서 알림(Notification)을 보내고, 소리 재생 -> Service 이용
+        // 1. Service Intent 생성, Service 실행
+        val serviceIntent = Intent(context, SoundPlayService::class.java)
+        serviceIntent.putExtra("state", receivedState)
+        context.startService(serviceIntent)
     }
 }
---
+++ AlarmApp/app/src/main/java/com/example/alarmapp/SoundPlayService.kt
@@ -0,0 +1,44 @@
+package com.example.alarmapp
+
+import android.app.Service
+import android.content.Intent
+import android.media.Ringtone
+import android.media.RingtoneManager
+import android.os.IBinder
+
+class SoundPlayService : Service() {
+    companion object {
+        var ringtone : Ringtone? = null
+    }
+    // 한 번 만들어지면 계속 실행됨
+    override fun onBind(intent: Intent): IBinder? {
+        return null
+    }
+
+    // 초기에 실행될 때 한번만 호출
+    override fun onCreate() {
+        ringtone = RingtoneManager.getRingtone(applicationContext, RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM))
+
+    }
+
+    // 실행될 때마다 호출
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val receivedState = intent?.getStringExtra("state")
+
+        if(receivedState == "ON") {
+            ringtone?.play()
+        } else {
+            ringtone?.stop()
+        }
+
+//        return super.onStartCommand(intent, flags, startId) // Default : START_STICKY
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        if(ringtone != null) {
+            ringtone?.stop()
+        }
+        super.onDestroy()
+    }
+}
