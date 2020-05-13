c3fe2a32f8983e0bb1476c967f7988a44c6db83b
rekafekili <rekafekili@gmail.com>
2020-05-13 14:56:09 +0900

    9.1.3 : Handler 에서 데이터 처리하기 - Bundle, what/obj

--- MyThread/app/src/main/java/com/example/mythread/MainActivity.kt
+++ MyThread/app/src/main/java/com/example/mythread/MainActivity.kt
@@ -10,13 +10,19 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 // Note : Ctrl + Alt + F - 핃드 생성
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val SUCCESS = 101
+    }
+
     private var count: Int = 0
     private val uiHandler: UIHandler = UIHandler()
 
     // (1) UI 쓰레드에서 시간이 걸리는 작업을 하면 안된다. -> Thread 필요
     // (2) Thread 에서는 UI Thread 의 View를 조작할 수 없음
     // (3) 쓰레드간 데이터 전달 방법이 필요 -> runOnUiThread, Handler...
-    // (3-1) Handler 클래스 확장 사용
+    // (Usage 1) Handler 클래스 확장 사용
+    //   1. Handler.obtainMessage()
+    //   2. Handler.obtainMessage(what, obj)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,25 +37,58 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
     inner class MyThread : Thread() {
         override fun run() {
-            for(i in 0 until 5) {
+            for (i in 0 until 5) {
                 sleep(1000)
-                Log.d("[     Thread Test     ]", "count = ${++count}")
 //                main_button_up.text = (count).toString() // -> CalledFromWrongThreadException 발생
 
 //                runOnUiThread { // UI Thread에서 실행하도록 해준다.
-//                    main_textview.text = (count).toString() // -> CalledFromWrongThreadException 발생
+//                    main_textview.text = (count).toString()
 //                }
 
-                // UI 업데이트를 하고 싶을 때 Handler 를 이용해서 Message 를 보내자.
+                /**
+                 * (Usage 1-1) UI 업데이트를 하고 싶을 때 Handler 를 이용해서 Message 를 보내자.
+                 */
+//                val message = uiHandler.obtainMessage()
+//                val bundle = Bundle()
+//                bundle.putInt("count", ++count)
+//                message.data = bundle
+//                uiHandler.sendMessage
+
+                /**
+                 * (Usage 1-2) obtainsMessage(what, obj); int, object
+                 */
+                count++
+                val message = uiHandler.obtainMessage(SUCCESS, Person(age = 20+count))
+                // Bundle 데이터를 담을 수 있지만, what, obj 만 사용하는 예제로
+                message.sendToTarget()
+
+
+                Log.d("[     Thread Test     ]", "count = $count")
             }
         }
     }
 
     inner class UIHandler : Handler() { // UI Thread 에 접근해서 조작할 수 있도록 하는 클래스
         override fun handleMessage(msg: Message) {
-            //
+            // 다른 Thread 에서 Message Queue 를 통해 받아온 메시지를 처리
+
+            // (1-1) 처리할 데이터가 하나만 있을 경우 Bundle을 사용한다.
+//            val receivedBundle = msg.data
+//            val value = receivedBundle.getInt("count")
+//            main_textview.text = (value).toString()
+
+            // (1-2) 여러 종류의 데이터를 처리하고 싶은 경우 what을 사용하여 분기한다.
+            when(msg.what) {
+                SUCCESS -> {
+                    val person = msg.obj as Person
+                    main_textview.text = "${person.name}'s Age = ${person.age}"
+                }
+            }
         }
     }
+
+    data class Person (val age: Int = 20, val name: String = "홍길동")
 }
