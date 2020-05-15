34f07685753b7ce8a6328cca787444df1a4776ee
rekafekili <rekafekili@gmail.com>
2020-05-15 12:10:18 +0900

    9.2.3 : Add Record and Reset

--- MyStopwatch/app/src/main/java/com/example/mystopwatch/MainActivity.kt
+++ MyStopwatch/app/src/main/java/com/example/mystopwatch/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.mystopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import org.w3c.dom.Text
 import java.util.*
 import kotlin.concurrent.timer
 
@@ -10,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     private var time = 0
     private var isRunning = false
     private var timerTask: Timer? = null
+    private var lap = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +21,50 @@ class MainActivity : AppCompatActivity() {
 
         main_action_floationbutton.setOnClickListener {
             isRunning = !isRunning
-            if(isRunning) {
+            if (isRunning) {
                 start()
             } else {
                 pause()
             }
         }
+
+        main_laps_floationbutton.setOnClickListener {
+            if (isRunning) {
+                recordLapTime()
+            } else {
+                Toast.makeText(this, "Please Play Stopwatch", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        main_refresh_floationbutton.setOnClickListener {
+            reset()
+        }
+    }
+
+    private fun reset() {
+        timerTask?.cancel()
+        time = 0
+        isRunning = false
+        lap = 0
+        main_action_floationbutton.setImageResource(R.drawable.ic_play_arrow_black_24dp)
+        main_lap_list_linearlayout.removeAllViews()
+//        main_second_textview.text = "0"
+//        main_millisec_textview.text = ".00"
+
+        /* TextView 에서 Post */
+        main_second_textview.post {
+            main_second_textview.text = "0"
+            main_millisec_textview.text = ".00"
+        }
+    }
+
+    private fun recordLapTime() {
+        val lapTime = time
+        val textView = TextView(this)
+        textView.text = "${++lap} LAP : ${lapTime / 100}.${lapTime % 100}"
+        textView.textSize = 30F
+        textView.textAlignment = TextView.TEXT_ALIGNMENT_CENTER
+        main_lap_list_linearlayout.addView(textView, 0) // 최상단 배치
     }
 
     private fun start() {
@@ -36,7 +78,7 @@ class MainActivity : AppCompatActivity() {
 
             runOnUiThread {
                 main_second_textview.text = second.toString()
-                main_millisec_textview.text = millis.toString()
+                main_millisec_textview.text = ".${millis}"
             }
         }
     }
