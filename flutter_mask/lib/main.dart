a76f6a3e287366ff27ffabbf0182b6f029b9948b
rekafekili <rekafekili@gmail.com>
2020-05-29 17:20:22 +0900

    Flutter : Main UI 작성

--- flutter_mask/lib/main.dart
+++ flutter_mask/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:fluttermask/model/store.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
@@ -28,27 +29,51 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // 비동기 async - await
+  final stores = List<Store>();
+
+  // 비동기 async(Future 반환) - await
+  // 비동기 메소드의 반환값은 Future가 되어야 하며, 특정 타입을 지정할 때는 Future<int> 같이 제네릭을 사용한다.
   Future fetch() async {
     var url =
         "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json?lat=37.266389&lng=126.999333&m=5000";
+
+    // await를 사용하여 해당 호출이 완료될때까지 기다려준다.(비동기 안에서 동기화 해준다.)
     var response = await http.get(url);
 
-    print('Response status : ${response.statusCode}');
-    print('Response body : ${jsonDecode(utf8.decode(response.bodyBytes))}');
+    final jsonResult = jsonDecode(utf8.decode(response.bodyBytes));
+
+    final jsonStores = jsonResult['stores'];
+
+    // setState : 화면을 갱신하도록 요청하는 메소드
+    setState(() {
+      stores.clear();
+      jsonStores.forEach((jsonStore) {
+        stores.add(Store.fromJson(jsonStore));
+      });  
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("title"),
-        ),
-        body: Center(
-          child: RaisedButton(
-            onPressed: fetch,
-            child: Text('Button'),
-          ),
-        ));
+      appBar: AppBar(
+        title: Text("마스크 재고 있는 곳 : 0곳"),
+      ),
+      body: ListView(
+        children: stores.map((e) {
+          return ListTile(
+            title: Text(e.name),
+            subtitle: Text(e.addr),
+            trailing: Text(e.remainStat ?? '매진'),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetch();
   }
 }
