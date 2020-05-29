cd9c757869077c609933440f25c7aee27d467e8d
rekafekili <rekafekili@gmail.com>
2020-05-29 15:55:25 +0900

    Flutter : Async HTTP 통신 Response 받기

--- flutter_mask/lib/main.dart
+++ flutter_mask/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -11,19 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
@@ -37,14 +28,27 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // 비동기 async - await
+  Future fetch() async {
+    var url =
+        "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json?lat=37.266389&lng=126.999333&m=5000";
+    var response = await http.get(url);
+
+    print('Response status : ${response.statusCode}');
+    print('Response body : ${jsonDecode(utf8.decode(response.bodyBytes))}');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("title"),),
-      body: Center(
-        child: Text('text'),
-      )
-    );
+        appBar: AppBar(
+          title: Text("title"),
+        ),
+        body: Center(
+          child: RaisedButton(
+            onPressed: fetch,
+            child: Text('Button'),
+          ),
+        ));
   }
 }
-
