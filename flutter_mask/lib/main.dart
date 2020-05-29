f4ae429d8375e7c64a9c19a4f7ad4a3d37ba2f4d
rekafekili <rekafekili@gmail.com>
2020-05-29 17:44:48 +0900

    Flutter : 판매수량 UI 수정

--- flutter_mask/lib/main.dart
+++ flutter_mask/lib/main.dart
@@ -79,10 +79,47 @@ class _MyHomePageState extends State<MyHomePage> {
                 return ListTile(
                   title: Text(e.name),
                   subtitle: Text(e.addr),
-                  trailing: Text(e.remainStat ?? '매진'),
+                  trailing: _buildRemainStatWidget(e),
                 );
               }).toList(),
             ),
+    );
+  }
+
+  Widget _buildRemainStatWidget(Store store) {
+    var remainStat = '판매중지';
+    var description = '판매중지';
+    var color = Colors.black;
+
+    switch (store.remainStat) {
+      case 'plenty':
+        remainStat = '충분';
+        description = '100개 이상';
+        color = Colors.green;
+        break;
+      case 'some':
+        remainStat = '보통';
+        description = '30 ~ 100개';
+        color = Colors.yellow;
+        break;
+      case 'few':
+        remainStat = '부족';
+        description = '2 ~ 30개';
+        color = Colors.red;
+        break;
+      case 'empty':
+        remainStat = '소진임박';
+        description = '1개 이하';
+        color = Colors.grey;
+        break;
+    }
+
+    return Column(
+      children: <Widget>[
+        Text(remainStat,
+            style: TextStyle(color: color, fontWeight: FontWeight.bold)),
+        Text(description, style: TextStyle(color: color)),
+      ],
     );
   }
 
