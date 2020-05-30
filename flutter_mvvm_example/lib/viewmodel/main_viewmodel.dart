6b1933f4bd38c68da3b326fd34679803667c2c5e
rekafekili <rekafekili@gmail.com>
2020-05-30 11:53:30 +0900

    Learn about TextField for Input in Flutter

---
+++ flutter_mvvm_example/lib/viewmodel/main_viewmodel.dart
@@ -0,0 +1,27 @@
+//import 'dart:async';
+//
+//abstract class SubscriptionViewModel {
+//  Sink get inputMailText;
+//  Stream<bool> get outputIsButtonEnabled;
+//  Stream<String> get outputErrorText;
+//
+//  void dispose();
+//}
+//
+//class SubscriptionViewModelImpl implements SubscriptionViewModel {
+//  var _mailTextController = StreamController<String>.broadcast();
+//
+//  @override
+//  Sink get inputMailText => _mailTextController;
+//
+//  @override
+//  Stream<bool> get outputIsButtonEnabled => _mailTextController.stream
+//      .map((email) => EmailValidater.isEmailValid(email));
+//
+//  @override
+//  Stream<String> get outputErrorText => outputIsButtonEnabled
+//      .map((isEnabled) => isEnabled ? null : "Invalid email");
+//
+//  @override
+//  void dispose() => _mailTextController.close()
+//}
